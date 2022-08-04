aecc926c3d05b806212059f5fb90a1f02455e282
Jasper van Herpt <jasper.van.herpt@omines.com>
2022-08-05 00:50:55 +0200

    Add test with local DB connection
    Bad test, uses local resources, but sqlite cannot be used because of postgres explicit function calls and sqlmock is too specific.

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/jaztec/gorm-repository
 go 1.18
 
 require (
-	github.com/sirupsen/logrus v1.9.0
 	gorm.io/driver/postgres v1.3.8
+	gorm.io/driver/sqlite v1.3.6
 	gorm.io/gorm v1.23.8
 )
 
@@ -18,8 +18,8 @@ require (
 	github.com/jackc/pgtype v1.11.0 // indirect
 	github.com/jackc/pgx/v4 v4.16.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- repository_test.go
+++ repository_test.go
@@ -1,1 +1,55 @@
 package repository_test
+
+import (
+	"context"
+	repository "github.com/jaztec/gorm-repository"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"testing"
+)
+
+type testModel struct {
+	repository.Model
+
+	Name string
+}
+
+func TestCRUDCommands(t *testing.T) {
+	db := getDb(t)
+	r, err := repository.NewRepository[testModel](db)
+	if err != nil {
+		t.Fatalf("Error creating DB: %v", err)
+	}
+
+	t.Run("Create one", func(t *testing.T) {
+		n := testModel{
+			Name: "Test",
+		}
+
+		ne, err := r.Create(context.Background(), &n)
+		if err != nil {
+			t.Errorf("Error creating one: %+v", err)
+		}
+		if ne.ID == "" {
+			t.Errorf("No ID was filled")
+		}
+		if ne.Name != "Test" {
+			t.Errorf("Name does not comply: %s", ne.Name)
+		}
+	})
+}
+
+func getDb(t *testing.T) *gorm.DB {
+	db := postgres.Open("postgres://test:test@localhost:5432")
+	gdb, err := gorm.Open(db, &gorm.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var m testModel
+	if err := gdb.Migrator().AutoMigrate(&m); err != nil {
+		t.Fatal(err)
+	}
+
+	return gdb
+}
